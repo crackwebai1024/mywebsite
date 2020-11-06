bde288e95c6a61d09073debe627b10e04d95472b
crackwebai1024 <webtowardapex@outlook.com>
2020-11-06 18:31:36 +0800

    add some function

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,67 +1,82 @@
-import React, { Component } from 'react';
-import Image from './Image'
+import React, { Component } from "react";
+import Image from "./Image";
+import ModalImage from "react-modal-image";
 
 class About extends Component {
-   state = {
-      showImage: false
-   }
+  state = {
+    showImage: false,
+  };
 
-   showProfileImage = () => {
-      debugger
-      this.setState({
-         showImage: !this.state.showImage
-      })
-   }
-   render() {
-
-   if(this.props.data){
+  showProfileImage = () => {
+    debugger;
+    this.setState({
+      showImage: !this.state.showImage,
+    });
+  };
+  render() {
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
-   }
+    }
 
-   return (
+    return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-               <img className="profile-pic"  src={profilepic} alt="Mingxing Wang Profile Pic" 
-               onClick={this.showProfileImage}/>
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
+        <div className="row">
+          <div className="three columns">
+            <ModalImage
+              className="profile-pic"
+              small={profilepic}
+              large={profilepic}
+              alt="Mingxing"
+            />
+          </div>
+          <div className="nine columns main-col">
+            <div className="aboutme">
+              <h2>About Me</h2>
 
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                     <span>{name}</span><br />
-                     <span>{street}<br />
-                           {city} {state}, {zip}
-                  </span><br />
-                     <span>{phone}</span><br />
-                     <span>{email}</span>
-                  </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+              <p className="bio">{bio}</p>
             </div>
-         </div>
-      </div>
-
-   </section>
-   );
-}
+            <div className="row">
+              <div
+                className="contact-details aboutme columns"
+                style={{ marginRight: 0 }}
+              >
+                <h2>Contact Details</h2>
+                <p className="address bio">
+                  <span>{name}</span>
+                  <br />
+                  <span>
+                    {street}
+                    <br />
+                    {city} {state}, {zip}
+                  </span>
+                  <br />
+                  <span>{phone}</span>
+                  <br />
+                  <span>{email}</span>
+                </p>
+              </div>
+              <div className="columns download">
+                <p>
+                  <a href={resumeDownload} className="button">
+                    <i className="fa fa-download"></i>Download Resume
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default About;
