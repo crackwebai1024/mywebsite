2831fb89b054a077ecc75077ee56cb886f0b6cdf
crackwebai1024 <webtowardapex@outlook.com>
2020-12-25 14:54:30 +0800

    add laravel

---
+++ src/Test/ProModeButton.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { Component } from "react";
+import { ProModeContext } from "./ProModeContext";
+
+// export class ActionButton extends Component {
+//   /* test for using consumer in context */
+//   //   render() {
+//   //     return (
+//   //       <ProModeContext.Consumer>
+//   //         {(contextData) => (
+//   //           <button onClick={contextData.toggle}>
+//   //             {contextData.proModer.toString()}
+//   //           </button>
+//   //         )}
+//   //       </ProModeContext.Consumer>
+//   //     );
+//   //   }
+//   /**testing for simplified api */
+//   //   static contextType = ProModeContext;
+//   //   render() {
+//   //     return (
+//   //       <button onClick={this.context.toggle}>
+//   //         {this.context.proModer.toString()}
+//   //       </button>
+//   //     );
+//   //   }
+// }
+
+export function ActionButton() {
+  const context = useContext(ProModeContext);
+  return (
+    <button onClick={context.toggle}>{context.proModer.toString()}</button>
+  );
+}
---
+++ src/Test/ProModeContext.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const ProModeContext = React.createContext({});
